0e678ebfa047307c7c5f4be1d8e6a6e209759ff5
Andre Ziviani <andrepziviani@gmail.com>
2023-06-13 19:42:20 -0300

    fix: AWS Health service region

--- exporter/aws.go
+++ exporter/aws.go
@@ -3,15 +3,19 @@ package exporter
 import (
 	"context"
 	"fmt"
+	"net"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/health"
 )
 
 const (
-	TermOnDemand string = "JRTCKXETXF"
-	TermPerHour  string = "6YS6EN2CT7"
+	TermOnDemand   string = "JRTCKXETXF"
+	TermPerHour    string = "6YS6EN2CT7"
+	HealthEndpoint string = "global.health.amazonaws.com"
 )
 
 type Pricing struct {
@@ -48,13 +52,36 @@ type Details struct {
 	PricePerUnit map[string]string
 }
 
-func newAWSConfig(ctx context.Context) (aws.Config, error) {
+func (m *Metrics) NewHealthClient(ctx context.Context) {
+	// AWS Health is a global service with two regions:
+	// Active: us-east-1
+	// Passive: us-east-2
+	// When theres an incident in us-east-1 AWS can change the endpoint to us-east-2 but, AFAIK you have to manage this yourself
+	// AWS Health Aware implementation also do something like this...
+	cname, err := net.LookupCNAME(HealthEndpoint)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	cname = strings.TrimSuffix(cname, ".")
+	region := strings.Split(cname, ".")[1]
+
+	cfg, err := newAWSConfig(ctx, config.WithRegion(region))
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	m.health = health.NewFromConfig(cfg, health.WithEndpointResolver(health.EndpointResolverFromURL(fmt.Sprintf("https://%s", cname))))
+}
+
+func newAWSConfig(ctx context.Context, optFns ...func(*config.LoadOptions) error) (aws.Config, error) {
 	region := os.Getenv("AWS_REGION")
 	if region == "" {
 		return aws.Config{}, fmt.Errorf("Please configure the AWS_REGION environment variable")
 	}
 
-	cfg, err := config.LoadDefaultConfig(ctx)
+	cfg, err := config.LoadDefaultConfig(ctx, optFns...)
 	if err != nil {
 		return aws.Config{}, err
 	}
